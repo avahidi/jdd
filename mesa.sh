3f9734eca7fbaa33d3cec0c05e00c5a219fbb300
Vahidi, Arash <arash.vahidi@gmail.com>
2025-07-04 01:08:57 +0200

    removed perofmance.txt, added mesa

---
+++ mesa.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo "Recording execution time with mesa. This will take some time, go get some coffee..."
+echo "See https://github.com/avahidi/mesa"
+
+TAGS=`git log -1 --oneline || echo "No git?"`
+MESA="mesa --filter=exact"
+JAVA="java -cp target/classes -Xmx512M"
+
+#
+$MESA --note="$TAGS" --warmups=2 --runs=10 -- $JAVA jdd.examples.BDDQueens 11
+$MESA --note="$TAGS" --warmups=2 --runs=10 -- $JAVA jdd.examples.ZDDQueens 12
+$MESA --note="$TAGS" --warmups=2 --runs=10 -- $JAVA jdd.examples.ZDDCSPQueens 13
+
+$MESA --note="$TAGS" --warmups=1 --runs=5 -- $JAVA jdd.examples.Adder 1024
+$MESA --note="$TAGS" --warmups=1 --runs=2 -- $JAVA jdd.bdd.debug.BDDTraceSuite data/yangs_traces.zip 10240
